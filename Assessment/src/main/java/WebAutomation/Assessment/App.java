13a8614ad5d7294c46bafe6ede72add119655a66
Bhogaraju <BhogaraM@boral.com.au>
2020-04-14 15:23:00 +1000

    testcase update

--- Assessment/src/main/java/WebAutomation/Assessment/App.java
+++ Assessment/src/main/java/WebAutomation/Assessment/App.java
@@ -1,67 +1,32 @@
 package WebAutomation.Assessment;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
-
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.interactions.Actions;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Parameters;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class App 
+public class App extends Generic
 {
-	WebDriver driver=null;
-	
-	@Parameters({"browser"})
-	@BeforeTest
-    public void openBrowser(String browser) {
-		if(browser.equals("chrome")) {
-			System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
-	        driver = new ChromeDriver();
-	        System.out.println("launched chrome browse");
-		} else if (browser.equalsIgnoreCase("firefox")) {
-			System.setProperty("webdriver.gecko.driver", "./drivers/geckodriver.exe");
-			driver = new FirefoxDriver();
-		} else if (browser.equalsIgnoreCase("ie")) {
-			System.setProperty("webdriver.ie.driver", "./drivers/IEDriverServer.exe");
-			driver = new InternetExplorerDriver();
-	        System.out.println("launched ie browse");
-		}
-    	
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS); 
-    }
-    
-    @Test
+	@Test
     public void test() throws InterruptedException {
-    	driver.get("https://www.amazon.com/");
-    	
-    	driver.findElement(By.xpath("//*[@id=\"input_1\"]")).sendKeys("BhogaraM");
-    	driver.findElement(By.xpath("//*[@id=\"input_2\"]")).sendKeys("B0ral123");
-    	driver.findElement(By.xpath("//*[@id=\"submit\"]")).click();
-    	Thread.sleep(3000);
-    	
-    	Actions action = new Actions(driver);
-    	WebElement we = driver.findElement(By.id("nav-link-accountList"));
-    	action.moveToElement(we).build().perform();
-    	driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
-    	driver.findElement(By.className("nav-action-inner")).click();
-    	Thread.sleep(2000);
-    	driver.findElement(By.id("ap_email")).sendKeys("mounicabhogaraju1995@gmail.com");
-    	driver.findElement(By.id("continue-announce")).click();
-    	driver.findElement(By.id("ap_password")).sendKeys("mounil007");
-    	
-    	
-    }
-    
-    @AfterTest
-    public void CloseBrowser() {
-    	driver.close();
+    	LaunchApplication();
+    	Login();
+    	ClearCart();
+    	SearchItem();
+    	SelectItem();
+    	ItemInfo itemInfo = ExtractItemInfo();
+    	AddItemToCart();
+    	ProceedToCheckout();
+    	ItemInfo itemCheckoutInfo = ExtractCheckoutItemInfo();
+    	Assert.assertEquals(itemInfo.title, itemCheckoutInfo.title, "titles are equal");
+    	Assert.assertEquals(itemInfo.price, itemCheckoutInfo.price, "prices are equal");
+    	Signout();
     }
 }
